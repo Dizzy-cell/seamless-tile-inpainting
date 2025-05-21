be1898f52b600d2efe122a51b3397349f38c654c
Dizzy <65171430+Dizzy-cell@users.noreply.github.com>
2025-05-21 14:32:17 +0800

    Update blender_mask.py

--- blender_mask.py
+++ blender_mask.py
@@ -36,43 +36,50 @@ if not vg:
 width, height = image_size
 mask_image = np.zeros((height, width), dtype=np.uint8)
 
-# 遍历所有面
-for face_index, poly in enumerate(me.polygons):
-    points = []
-    for loop_index in poly.loop_indices:
-        loop = me.loops[loop_index]
-        uv = uv_layer[loop.index].uv
-        x = int(uv.x * (width - 1))
-        y = int((1.0 - uv.y) * (height - 1))  # Flip Y to match image coordinates
+vertex_groups = obj.vertex_groups
+
+for vg in vertex_groups:
+    group_name = vg.name
+    txt.write(f" -> 正在处理顶点组: {group_name}\n")
+    
+    width, height = image_size
+    mask_image = np.zeros((height, width), dtype=np.uint8)
+    # 遍历所有面
+    for face_index, poly in enumerate(me.polygons):
+        points = []
+        for loop_index in poly.loop_indices:
+            loop = me.loops[loop_index]
+            uv = uv_layer[loop.index].uv
+            x = int(uv.x * (width - 1))
+            y = int((1.0 - uv.y) * (height - 1))  # Flip Y to match image coordinates
+            
+            try:
+                vert_idx = loop.vertex_index
+                weight = vg.weight(vert_idx)
+            except:
+                continue
+
+            if weight > 0.5:
+                if 0 <= x < width and 0 <= y < height:
+                    points.append((x, y))
         
-        try:
-            vert_idx = loop.vertex_index
-            weight = vg.weight(vert_idx)
-        except:
-            continue
+        txt.write(f"{points}\n")
+        
+        # 如果有至少三个点，进行填充
+        if len(points) >= 3:
+            # 创建临时图像用于绘制多边形
+            temp_img = Image.new("L", (width, height), 0)
+            draw = ImageDraw.Draw(temp_img)
+            draw.polygon(points, fill=255)  # 每个面一个编号（从 1 开始）
+            mask_data = np.array(temp_img)
+            mask_image[mask_data > 0] =  255 # 保留编号信息
 
-        if weight > 0.5:
-            if 0 <= x < width and 0 <= y < height:
-                points.append((x, y))
-    
-    txt.write(f"{points}\n")
-    
-    # 如果有至少三个点，进行填充
-    if len(points) >= 3:
-        # 创建临时图像用于绘制多边形
-        temp_img = Image.new("L", (width, height), 0)
-        draw = ImageDraw.Draw(temp_img)
-        draw.polygon(points, fill=255)  # 每个面一个编号（从 1 开始）
-        mask_data = np.array(temp_img)
-        mask_image[mask_data > 0] =  255 # 保留编号信息
 
-print("dd")
+    #np.save(f"{output_path}/mask.npy", mask_image)
 
-np.save(f"{output_path}/mask.npy", mask_image)
+    ## 保存图像
 
-## 保存图像
+    img = Image.fromarray(mask_image, mode='L')
+    img.save(f"/Users/honor/Downloads/future/MiniCPM-o/mask_{group_name}.png")
 
-img = Image.fromarray(mask_image, mode='L')
-img.save(f"/Users/honor/Downloads/future/MiniCPM-o/mask_sleeve.png")
-
-print("顶点组 Mask 图像已保存！")
+    txt.write("顶点组 Mask 图像已保存！")
